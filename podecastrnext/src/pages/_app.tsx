5445e84830a34a64039c267ba977749dd49643dd
LecioVilela <lecio.vilela@yahoo.com>
2021-04-22 17:17:12 -0300

    upload project

--- podecastrnext/src/pages/_app.tsx
+++ podecastrnext/src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import styles from '../styles/app.module.scss'
 import '../styles/global.scss'
 import {AppProps} from 'next/app';
+import { Header } from '../components/Header';
+import {Player} from '../components/Player'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+      <Header/> 
+      <Component {...pageProps} />
+      </main>
+      <Player/>
+      
+    </div>
+  )
 }
 
 export default MyApp
